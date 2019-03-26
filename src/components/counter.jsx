f79e81b396eeebc49f7ad4cc4760db45b0a0d36a
Carlos <careyes4820@ealge.fgcu.edu>
2019-03-25 22:00:12 -0400

    Initial Commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    //console.log("prevProps", prevProps);
+    //console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new data from the server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+
+  render() {
+    console.log("Counter - Rendered");
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        <br />
+        {/*
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+        */}
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: val } = this.props.counter;
+    return val === 0 ? "Zero" : val;
+  }
+}
+
+export default Counter;
